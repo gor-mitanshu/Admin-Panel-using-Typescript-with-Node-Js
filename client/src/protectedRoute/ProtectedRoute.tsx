256ef79edcb97a03352ae7e0828d1bda79526ebd
Gor Mitanshu <gor.mitanshu3@gmail.com>
2023-07-26 16:06:19 +0530

    issue solved of authentication on reload

--- client/src/protectedRoute/ProtectedRoute.tsx
+++ client/src/protectedRoute/ProtectedRoute.tsx
@@ -5,10 +5,13 @@ import { Navigate, useLocation } from "react-router-dom";
 const ProtectedRoute = ({ children }: any) => {
   const { authed }: any = useAuth();
   const { location }: any = useLocation();
+  const checkToken = localStorage.getItem("auth");
 
   return (
     <>
       {authed === true ? (
+        children
+      ) : checkToken ? (
         children
       ) : (
         <Navigate to={"/login"} replace state={location?.pathname} />
