63f93eb7be6afa4fdfc87ee137e4a656dcef269a
Gor Mitanshu <gor.mitanshu3@gmail.com>
2023-07-27 12:29:51 +0530

    done the task of showing the data from token's obj

--- client/src/components/Navbar/Navbar.tsx
+++ client/src/components/Navbar/Navbar.tsx
@@ -2,20 +2,47 @@ import React, { useState } from "react";
 import { AppBar, Grid, Toolbar, Typography } from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
 import "../Navbar/Navbar.css";
+import { useSelector } from "react-redux";
+import jwtDecode from "jwt-decode";
 
+interface NavbarProps {
+  decodeToken?: {};
+  user: {
+    firstname: string;
+    lastname: string;
+    role: string;
+  };
+}
 const Navbar = ({ toogleSidebar }: any): JSX.Element => {
   const [isOpen, setOpen] = useState<boolean>(false);
   const toogleSiderbar = () => {
     setOpen(!isOpen);
     toogleSidebar(isOpen);
   };
+  const token = useSelector((state: any) => state.LoginAuthReducer.token);
+  const decodeToken: NavbarProps = jwtDecode(token);
+  const name = decodeToken.user.firstname + " " + decodeToken.user.lastname;
+  const role = decodeToken.user.role;
   return (
     <div>
       <Grid className="navbar">
         <AppBar>
           <Toolbar className="toolbar-navbar">
             <MenuIcon className="menu-icon" onClick={toogleSiderbar} />
-            <Typography className="navbar-title">Admin Panel</Typography>
+            <Grid
+              sx={{
+                display: "flex",
+                justifyContent: "space-between",
+                alignItems: "center",
+                width: "100%",
+              }}
+            >
+              <Typography className="navbar-title">Panel</Typography>
+              <Typography className="navbar-name">
+                <span style={{ color: "#cacaca" }}>{role.toUpperCase()}</span>:{" "}
+                <span style={{ color: "#ff4382" }}>{name.toUpperCase()}</span>
+              </Typography>
+            </Grid>
           </Toolbar>
         </AppBar>
       </Grid>
