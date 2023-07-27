ad4c74e7ab15b560d71aef4ca450aaf4810bc7a7
Gor Mitanshu <gor.mitanshu3@gmail.com>
2023-07-27 16:55:00 +0530

    one way to redirect to login after token expired

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import LoginPage from "pages/Login/LoginPage";
@@ -6,8 +6,25 @@ import Layout from "components/Layout/Layout";
 import Dashboard from "pages/Dashboard/Dashboard";
 import { AuthProvider } from "context/authContext";
 import ProtectedRoute from "protectedRoute/ProtectedRoute";
+import { useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
+import jwtDecode from "jwt-decode";
+import { logout } from "redux/Action";
 
 function App() {
+  const dispatch = useDispatch();
+  const token = useSelector((state: any) => state.LoginAuthReducer.token);
+  useEffect(() => {
+    if (!!token) {
+      const decodeToken: any = jwtDecode(token);
+      const currentTime = Date.now() / 1000;
+      if (decodeToken.exp < currentTime) {
+        // alert("Your session has expired. Please try logging in again.");
+        dispatch<any>(logout());
+      }
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
   return (
     <>
       <AuthProvider>
