1c69840f24999924b92b34efb416cde923e62b12
Gor Mitanshu <gor.mitanshu3@gmail.com>
2023-07-31 11:18:34 +0530

    clear redux-persist done & issue of token exp remain

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,30 +1,48 @@
 import React, { useEffect } from "react";
 import "./App.css";
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import {
+  BrowserRouter,
+  Navigate,
+  Route,
+  Routes,
+  useNavigate,
+} from "react-router-dom";
 import LoginPage from "pages/Login/LoginPage";
 import Layout from "components/Layout/Layout";
 import Dashboard from "pages/Dashboard/Dashboard";
 import { AuthProvider } from "context/authContext";
 import ProtectedRoute from "protectedRoute/ProtectedRoute";
-// import { useDispatch } from "react-redux";
-// import { useSelector } from "react-redux";
-// import jwtDecode from "jwt-decode";
-// import { logout } from "redux/Action";
+import { useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
+import jwtDecode from "jwt-decode";
+import { logout } from "redux/Action";
+import { persistor } from "redux/Store";
 
 function App() {
-  // const dispatch = useDispatch();
-  // const token = useSelector((state: any) => state.LoginAuthReducer.token);
-  // useEffect(() => {
-  //   if (!!token) {
-  //     const decodeToken: any = jwtDecode(token);
-  //     const currentTime = Date.now() / 1000;
-  //     if (decodeToken.exp < currentTime) {
-  //       // alert("Your session has expired. Please try logging in again.");
-  //       dispatch<any>(logout());
-  //     }
-  //   }
-  //   // eslint-disable-next-line react-hooks/exhaustive-deps
-  // }, []);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const token = useSelector((state: any) => state.LoginAuthReducer.token);
+
+  useEffect(() => {
+    const checkTokenValidity = () => {
+      if (!!token) {
+        const decodeToken: any = jwtDecode(token);
+        const currentTime = Date.now() / 1000;
+        if (decodeToken.exp < currentTime) {
+          dispatch<any>(logout());
+          persistor.purge();
+          navigate("/login");
+        }
+      }
+    };
+    checkTokenValidity();
+    // Add an interval to check token validity every minute
+    const tokenCheckInterval = setInterval(checkTokenValidity, 60000);
+    return () => {
+      clearInterval(tokenCheckInterval);
+    };
+  }, [token, dispatch, navigate]);
+
   return (
     <>
       <AuthProvider>
