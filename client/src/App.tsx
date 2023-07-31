5015b923088360a1f3ae851612495a5470f78557
Gor Mitanshu <gor.mitanshu3@gmail.com>
2023-07-31 13:20:00 +0530

    Solved the issue of token expire and logged out by using another method

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import "./App.css";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import LoginPage from "pages/Login/LoginPage";
@@ -6,35 +6,8 @@ import Layout from "components/Layout/Layout";
 import Dashboard from "pages/Dashboard/Dashboard";
 import { AuthProvider } from "context/authContext";
 import ProtectedRoute from "protectedRoute/ProtectedRoute";
-import { useDispatch } from "react-redux";
-import { useSelector } from "react-redux";
-import jwtDecode from "jwt-decode";
-import { logout } from "redux/Action";
-import { persistor } from "redux/Store";
 
 function App() {
-  const dispatch = useDispatch();
-  const token = useSelector((state: any) => state.LoginAuthReducer.token);
-
-  useEffect(() => {
-    const checkTokenValidity = () => {
-      if (!!token) {
-        const decodeToken: any = jwtDecode(token);
-        const currentTime = Date.now() / 1000;
-        if (decodeToken.exp < currentTime) {
-          dispatch<any>(logout());
-          persistor.purge(); // Clear the Redux-persist store
-        }
-      }
-    };
-    checkTokenValidity();
-    // Add an interval to check token validity every minute
-    const tokenCheckInterval = setInterval(checkTokenValidity, 60000);
-    return () => {
-      clearInterval(tokenCheckInterval);
-    };
-  }, [token, dispatch]);
-
   return (
     <>
       <AuthProvider>
--- client/src/protectedRoute/ProtectedRoute.tsx
+++ client/src/protectedRoute/ProtectedRoute.tsx
@@ -1,25 +1,48 @@
-import React from "react";
+import React, { useEffect } from "react";
 import useAuth from "../context/authContext";
 import { Navigate, useLocation } from "react-router-dom";
 import { useSelector } from "react-redux";
+import axios from "axios";
+import { logout } from "redux/Action";
+import { persistor } from "redux/Store";
+import { useDispatch } from "react-redux";
 
 const ProtectedRoute = ({ children }: any) => {
   const { authed }: any = useAuth();
   const { location }: any = useLocation();
   // const checkToken = localStorage.getItem("auth");
-  const checkToken = useSelector((state: any) => state.LoginAuthReducer.token);
-  // console.log(
-  //   useSelector((state: any) => state.LoginAuthReducer),
-  //   "LOginAUthReducer"
-  // );
-  // console.log(
-  //   useSelector((state: any) => state.LoginAuthReducer.token),
-  //   "Token"
-  // );
+  const checkedToken = useSelector(
+    (state: any) => state.LoginAuthReducer.token
+  );
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const checkTokenValidity = async () => {
+      try {
+        await axios.get(`${process.env.REACT_APP_API}/api/protectedRoute`, {
+          headers: {
+            Authorization: `Bearer ${checkedToken}`,
+          },
+        });
+      } catch (error: any) {
+        if (error.response && error.response.status === 401) {
+          console.log(error.response.data.message);
+          dispatch<any>(logout());
+          persistor.purge();
+        }
+      }
+    };
+
+    checkTokenValidity();
+    const tokenCheckInterval = setInterval(checkTokenValidity, 60000);
+    return () => {
+      clearInterval(tokenCheckInterval);
+    };
+  }, [dispatch, checkedToken]);
 
   return (
     <>
-      {authed === true || checkToken ? (
+      {authed === true || checkedToken ? (
         children
       ) : (
         <Navigate to={"/login"} replace state={location?.pathname} />
