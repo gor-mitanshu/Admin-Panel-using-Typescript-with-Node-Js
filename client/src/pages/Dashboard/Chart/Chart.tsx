e29368e8d59bc5bab4c2bb38ee09b210b8ac08c9
Gor Mitanshu <gor.mitanshu3@gmail.com>
2023-08-03 15:44:05 +0530

    Basic Chart option than before one

--- client/src/pages/Dashboard/Chart/Chart.tsx
+++ client/src/pages/Dashboard/Chart/Chart.tsx
@@ -3,34 +3,18 @@ import ReactEcharts from "echarts-for-react";
 import { Card } from "@mui/material";
 
 const Chart = () => {
-  const customColors = [
-    "#ff7070",
-    "#4fc1e9",
-    "#f6bb42",
-    "#a0d468",
-    "#a8a7a7",
-    "#37bc9b",
-    "#e9573f",
-  ];
-
   const option = {
     title: {
       text: "Weekly Data",
       subtext: "July 2023",
-      center: "left",
+      center: "",
     },
     xAxis: {
       type: "category",
       data: ["Mon", "Tue", "Wed", "Thurs", "Fri", "Sat", "Sun"],
-      axisLabel: {
-        rotate: 45,
-      },
     },
     yAxis: {
       type: "value",
-      axisLabel: {
-        formatter: (value: any) => `${value}`,
-      },
     },
     series: [
       {
@@ -42,33 +26,11 @@ const Chart = () => {
         backgroundStyle: {
           color: "rgba(180, 180, 180, 0.2)",
         },
-        itemStyle: {
-          // Set custom colors for each bar
-          color: (params: any) => customColors[params.dataIndex],
-        },
       },
     ],
     tooltip: {
       trigger: "axis",
-      formatter: (params: any) => {
-        const value = params[0].value;
-        return `Custom Tooltip: ${value}`;
-      },
     },
-    dataZoom: [
-      {
-        type: "inside",
-        start: 0,
-        end: 100,
-      },
-      {
-        type: "slider",
-        start: 0,
-        end: 100,
-        height: 20,
-        bottom: 10,
-      },
-    ],
     legend: {
       data: ["Data"],
     },
